99e8c9203db68f593c2f34e38f36bca44ab3a97c
unknown <juleskevinnjiki@gmail.com>
2022-11-18 15:12:18 +0100

    initialisationet premier component

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{'dark': darkMode}">
+  <div class="bg-white dark:bg-dim-800">
+
+    
+    <div class="min-h-full">
+      
+      <div class=" grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!--Left side bar-->
+        <div class="md:block xs:col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            
+            <SidebarLeft/>
+          </div>
+        </div>
+        
+        <!--Main content-->
+        
+        
+        <!--Rigt side bar-->
+      </div>
+      
+    </div>
+
+    </div>
+
+
+
+</div>
+</template>
+<script  setup>
+  const darkMode =ref(false)
+</script>
---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <div class="p-2 my-2 hover:rounded-full hover:bg-red-300 w-min">
+            <nuxt-link  to="/">
+                <div class="h-8 w-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+    
