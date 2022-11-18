5782c6b286b8951f9a83324db59cf13a0d08d7cd
unknown <juleskevinnjiki@gmail.com>
2022-11-18 18:13:11 +0100

    creation du sidebarleft

---
+++ components/Sidebar/Left/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { HomeIcon } from "@heroicons/vue/24/solid"
+      
+      const { defaultTransition } = useTailwindConfig();
+      </script>
+<template>
+  <div class="h-screen flex flex-col">
+    
+
+    <div
+      class="p-2 my-2 rounded-full hover:bg-blue-100 w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <nuxt-link to="/">
+        <div class="h-8 w-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+      <template v-slot:name>
+        Home
+      </template>
+    </SidebarLeftTab>
+  </div>
+</template>
+
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,26 @@
+<template>
+    <Nuxt-link to="#" class=" flex items-center p-3 w-min hover:bg-gray-200 rounded-full"
+    :class="defaultTransition">
+        <div class=" h-6 w-8 text-dark"  >
+            <slot name="icon"></slot>
+        </div>
+        <div class="ml-3 mt-4 text-xl" :class="textClasses">
+            <slot name="name"></slot>
+
+        </div>
+    </Nuxt-link>  
+</template>
+
+<script setup>
+
+const {defaultTransition} = useTailwindConfig()
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+const textClasses = computed(()=> props.active ? 'font-semibold' : 'font-normal')
+</script>
