317708118e24cfaa9fc858146d895ecf14fa6721
unknown <juleskevinnjiki@gmail.com>
2022-11-22 16:26:41 +0100

    installation de Prisma et parametrage de la base de donnees

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,5 +1,15 @@
+import {sendError} from "h3"
+
 export default defineEventHandler(async (event) =>{
     const body = await useBody(event)
+
+    const { username, email, password, repeatPassword, name} = body
+
+    if(!username|| !email|| !password|| !repeatPassword|| !name){
+        return sendError(event, createError({
+            statusCode: 400, statusMessage: 'Invalid params'
+        }))
+    }
 
     return {
         body: body
